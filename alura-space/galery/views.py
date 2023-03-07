2faea143676cbcb48f2ff119c492e7a94e238ae8
Caique Ferian <caiquegsf@gmail.com>
2023-03-07 17:19:20 -0300

    Cria Model via DJANGO, e adiciona de forma dinâmica os valores do DB no template

--- alura-space/galery/views.py
+++ alura-space/galery/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from galery.models import Photography
 
 
 # Cria Os Controllers da aplicação aqui, que neste caso está sendo usado para renderizar arquivos HTML
 def index(request):
-    return render(request, "galery/index.html")
+    photographys = Photography.objects.all()
+    return render(request, "galery/index.html", {"cards": photographys})
 
 
 def image(request):
